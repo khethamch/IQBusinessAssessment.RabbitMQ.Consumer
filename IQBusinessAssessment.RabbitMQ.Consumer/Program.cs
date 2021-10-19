821ce909c15eb571e7cf42939870138f583c0112
khetha mchunu <khethamch@gmail.com>
2021-10-19 18:37:45 +0200

    Add project files.

---
+++ IQBusinessAssessment.RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IQBusinessAssessment.RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
